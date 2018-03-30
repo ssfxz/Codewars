4b11561bd48f4e0dc1b76cf78c883c8a63776204
LiuY <lyssfxz@126.com>
2018-03-31 00:13:54 +0800

    which are in

--- Which-are-in.js
+++ Which-are-in.js
@@ -1,2 +1,5 @@
 // https://www.codewars.com/kata/which-are-in
 
+function inArray(array1,array2){
+    return array1.filter(a1 => array2.some(a2 => a2.includes(a1))).sort();
+}
--- fixed-length-palindromes.js
+++ fixed-length-palindromes.js
@@ -1,2 +1,6 @@
 // https://www.codewars.com/kata/fixed-length-palindromes
 
+function palin(a,b){
+    const half = Math.pow(10, Math.floor((a - 1) / 2)) + b - 1 + '';
+    return parseInt(half + half.split('').reverse().join('').slice(a % 2));
+};
